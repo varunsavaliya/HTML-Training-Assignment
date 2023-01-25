96e95cbfb2b2120f4b37042257c6b29efa180cae
varun <varunsavaliya000@gmail.com>
2023-01-25 15:27:21 +0530

    assignment 1

---
+++ js/dashboard.js
@@ -0,0 +1,9 @@
+let container = document.getElementById("allBtn");
+let btns = container.getElementsByClassName("button");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    })
+}
